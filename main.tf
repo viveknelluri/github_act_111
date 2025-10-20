c576cf7c007003e3272a82ba367bca77d073ccb9
viveknelluri <nellurivivek47@gmail.com>
2025-10-20 12:43:27 -0500

    add everythin

--- main.tf
+++ main.tf
@@ -8,3 +8,13 @@ resource "aws_s3_bucket" "example" {
     Name = "ExampleBucket"
   }
 }
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-stat-12345bucketet"   # your S3 bucket
+    key            = "env/dev/terraform.tfstate"   # path inside S3
+    region         = "us-east-1"
+    #dynamodb_table = "terraform-locks"             # optional
+    #encrypt        = true
+  }
+}
+
